93e6a6737102d184e03efff9840a54ba5ceb7f0b
nishachoudhary111 <55096692+nishachoudhary111@users.noreply.github.com>
2021-07-06 00:22:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const display = document.getElementById('clock');
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
+
+function updateTime() {
+    const date = new Date();
+
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+
+
+
+    display.innerText=`${hour} : ${minutes} : ${seconds}`
+}
+
+function formatTime(time) {
+    if ( time < 10 ) {
+        return '0' + time;
+    }
+    return time;
+}
+
+function setAlarmTime(value) {
+    alarmTime = value;
+}
+
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert('Alarm set');
+        }
+    }
+}
+
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
+}
+
+setInterval(updateTime, 1000);
+
+function countwords(){
+    let noc= document.getElementById("content").value;
+    let tot_char=noc.length;
+    let now = document.getElementById("content").value;
+    now=now.match( /\w+/g );
+    now=now.length;
+    document.getElementById('showdata').innerHTML="The total Characters = "+ tot_char;
+    document.getElementById('showwordscount').innerHTML="The total Words = "+ now;
+    
+}
+
